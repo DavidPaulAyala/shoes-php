1d11179700ca4986364d278affb35fde259823a6
David Ayala <dpayala@hotmail.com>
2016-10-02 15:35:53 -0700

    add initial silex/html setup for brand

--- app/app.php
+++ app/app.php
@@ -48,7 +48,23 @@
     $app->get("/stores/{id}", function($id) use ($app) {
     $store = Store::find($id);
     return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
-});
+    });
+
+    $app->post("/brands", function() use ($app) {
+        $brand = new Brand($_POST['name']);
+        $brand->save();
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    $app->post("/delete_brands", function() use ($app) {
+    Brand::deleteAll();
+    return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/brands/{id}", function($id) use ($app) {
+    $brand = Brand::find($id);
+    return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'brands' => $brand->getStores(), 'all_brands' => Store::getAll()));
+    });
 
     return $app;
 ?>
