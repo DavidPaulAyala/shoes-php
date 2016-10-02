4413b677d8c59dcb1292c939167d0318b95fdf0c
David Ayala <dpayala@hotmail.com>
2016-10-02 15:27:32 -0700

    add initial silex/html setup for store

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Brands.php";
-    require_once __DIR__."/../src/Stores.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
 
     use Symfony\Component\Debug\Debug;
     Debug::enable();
@@ -15,18 +15,40 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
-      return $app['twig']->render("home.html.twig");
+        return $app['twig']->render("index.html.twig");
     });
 
-
-    $app->get("/test", function() use ($app) {
-      return 'test variables here';
+    $app->get("/stores", function() use ($app) {
+        return $app['twig']->render("stores.html.twig", array('stores' => Store::getAll()));
     });
+
+    $app->get("/brands", function() use ($app) {
+        return $app['twig']->render("brands.html.twig", array('brands' => Brand::getAll()));
+    });
+
+    $app->post("/stores", function() use ($app) {
+        $store = new Store($_POST['name']);
+        $store->save();
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post("/delete_stores", function() use ($app) {
+    Store::deleteAll();
+    return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/stores/{id}", function($id) use ($app) {
+    $store = Store::find($id);
+    return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
+});
 
     return $app;
 ?>
